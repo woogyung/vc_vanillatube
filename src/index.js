e7f9bd25a1cc867f319d3fff77f28c7a6f0538c0
Ken Huh <kenhuh@Kens-MacBook-Pro.local>
2017-08-27 15:05:05 +0900

    update boiler plate

--- src/index.js
+++ src/index.js
@@ -26,16 +26,38 @@ var breeds = [
 ];
 
 /*
- * AJAX 요청 보내기
- *
- * 사용자가 하나의 종을 선택했을때, 현재 페이지에 강아지 사진을 보여주세요.
- * 사용자가 다른 종을 선택하면, 사진을 교체해주세요.
- *
- * 강아지 사진 URL은 아래의 API에서 가져올 수 있습니다.
- * GET https://dog.ceo/api/breed/강아지종/images/random
- * (강아지종 - 위에 주어진 배열에 담긴 영문 String 값으로 교체해서 사용)
- *
- * 참고 API 문서: https://dog.ceo/dog-api/
+ * 웹페이지에 각각의 종을 보여주도록 수정하는 부분입니다.
+ * 다른 형식으로 바꾸셔도 좋습니다.
+ */
+var breedListEl = document.getElementById('breed-list');
+
+breeds.forEach(function (breed) {
+    var listEl = document.createElement('li');
+
+    listEl.textContent = breed;
+    breedListEl.appendChild(listEl);
+});
+
+
+
+/*
+ * 사용자가 하나의 강아지종을 click했을때
+ * 선택한 종을 event 객체에서 찾아서 fetchPhotoURL을 실행시키세요.
  */
 
-console.log('Hello, Ponyo!');
+
+
+/*
+ * AJAX 요청 핸들링
+ *
+ * 성공적으로 response를 받으면, 받은 데이터에서 사진 url을 찾아서 UI에 해당 사진을 보여주세요.
+ * 기존에 선택했던 사진이 있다면, 교체해야 합니다.
+ *
+ * 보너스: 한번 가져온 데이터는 다시 또 요청이 나가지 않도록 해주세요.
+ */
+function fetchPhotoURL (breed) {
+    var apiURL = 'https://dog.ceo/api/breed/' + breed + '/images/random';
+    $.get(apiURL, function (data) {
+        console.log('Success: ', data);
+    });
+}
