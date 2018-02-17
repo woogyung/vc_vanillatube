f9b1e47dbb30e9c27a6ca2a514230bcc1eff5cba
Ken Huh <ken123777@gmail.com>
2018-02-17 18:49:39 +0900

    V6 Router

---
+++ src/js/gorilla/Router.js
@@ -0,0 +1,35 @@
+import _ from 'lodash';
+
+function createRouteMap (routes) {
+  return _.mapValues(routes, function (r) {
+    if (!r.path) {
+      throw new Error('[Router 생성자 함수] 설정하려는 경로의 `path`를 입력해주세요.');
+    }
+
+    return r;
+  });
+}
+
+function Router (config) {
+  this.routeMap = createRouteMap(config);
+}
+
+Router.prototype.validateRoute = function (routeName) {
+  if (!this.routeMap[routeName]) {
+    throw new Error('[Router.prototype.validateRoute] 설정되지 않은 경로입니다.');
+  }
+};
+
+Router.prototype.set = function (routeName, data) {
+  this.validateRoute(routeName);
+
+  if (!data && typeof this.routeMap[routeName].path === 'function') {
+    throw new Error('[Router.prototype.set] 변경하시려는 경로의 파라미터값이 필요합니다.');
+  }
+
+  window.history.pushState({
+    name: routeName
+  }, routeName, '/#' + this.routeMap[routeName].path(data));
+};
+
+export default Router;
